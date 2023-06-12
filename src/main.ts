4c433ed8a486d85e3119201d94ccf32d1696c631
Luis Carlos Silva <roxisxprol@gmail.com>
2023-06-12 13:12:38 -0300

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bora Rachar')
+    .setDescription('API')
+    .setVersion('v1')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
