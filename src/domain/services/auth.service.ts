196b779d86edbc58f8ac5f2d5b3d64a5f153e7ed
hboppe <hanna.nascimento31@gmail.com>
2023-07-13 20:54:53 -0400

    feat: add AuthService

---
+++ src/domain/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { JwtService } from "@nestjs/jwt";
+import { PrismaService } from "./prisma.service";
+import { comparePass } from "../core/hashPassword";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (user) {
+      const isPasswordMathing = await comparePass(password, user.password);
+
+      if (isPasswordMathing) return { email: user.email };
+    }
+
+    return null;
+  }
+
+  async login(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    return {
+      token: this.jwtService.sign({
+        email: email,
+        subject: user.id,
+      }),
+    };
+  }
+}
