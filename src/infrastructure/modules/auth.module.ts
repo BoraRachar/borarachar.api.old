23da436fb67f6d697a14a937187a2d5004eb6162
hboppe <hanna.nascimento31@gmail.com>
2023-07-13 20:56:55 -0400

    feat: add AuthModule

--- src/infrastructure/modules/auth.module.ts
+++ src/infrastructure/modules/auth.module.ts
@@ -1,7 +1,24 @@
 import { Module } from "@nestjs/common";
 import { SocialStrategy } from "../../domain/core/auth/social.strategy";
+import { AuthService } from "src/domain/services/auth.service";
+import { LocalStrategy } from "src/domain/core/auth/local.strategy";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "src/application/controllers/auth.controller";
+import { PrismaModule } from "nestjs-prisma";
+import { PrismaService } from "../../domain/services/prisma.service";
+import "dotenv/config";
 
 @Module({
-  providers: [SocialStrategy],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: "2h" },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, PrismaService],
 })
 export class AuthModule {}
