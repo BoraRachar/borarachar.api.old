dbef9d683a3bf62c1d5c27d534f2a13bcd9b08dd
hboppe <hanna.nascimento31@gmail.com>
2023-07-13 20:55:17 -0400

    feat: add LocalStrategy

---
+++ src/domain/core/auth/local.strategy.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { AuthService } from "src/domain/services/auth.service";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: "email",
+      passwordField: "password",
+    });
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+
+    if (!user) throw new BadRequestException("Invalid email or password.");
+
+    return user;
+  }
+}
