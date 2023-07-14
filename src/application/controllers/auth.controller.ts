c6cd7d2d5b893374cd6c294ba627409eda91d770
hboppe <hanna.nascimento31@gmail.com>
2023-07-13 20:56:41 -0400

    feat: add route Login

--- src/application/controllers/auth.controller.ts
+++ src/application/controllers/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthService } from '../../domain/services/auth.service';
+import { LoginInfoDto } from "src/domain/dto/login.dto";
+import { LocalAuthGuard } from "src/domain/core/auth/local-auth.guard";
+import { ApiBadRequestResponse, ApiOkResponse } from "@nestjs/swagger";
 
-@Controller("auth")
-export class AuthController {}
+@Controller("login")
+export class AuthController {
+  constructor(private authService: AuthService){}
+
+  @Post()
+  @ApiOkResponse({ description: "User has sucessfully logged in." })
+  @ApiBadRequestResponse({ description: "Invalid email or password." })
+  @UseGuards(LocalAuthGuard)
+  async login(@Body() loginInfo: LoginInfoDto) {
+    return await this.authService.login(loginInfo.email)
+  }
+
+}
