4432ae5b78738e70748194b5d1c2ecb70724852d
Luis Carlos <luiscroxis@gmail.com>
2023-08-01 10:57:09 -0300

    fix: remove unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { AppModule } from "./infrastructure/modules/app.module";
 import { NestFactory } from "@nestjs/core";
-import type { SwaggerDocumentOptions } from "@nestjs/swagger";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import {
-  NestApplicationOptions,
-  RequestMethod,
-  ValidationPipe,
-} from "@nestjs/common";
+import { SwaggerModule } from "@nestjs/swagger";
+import { NestApplicationOptions, ValidationPipe } from "@nestjs/common";
 import "dotenv/config";
-import * as fs from "fs";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import * as http from "http";
-import * as https from "https";
 import * as express from "express";
 import { Logger } from "./common/helper/logger";
 import { ConfigService } from "./domain/services/config.service";
